2927f855844c45ca8b5ddc88cdcf39edf77872bc
Aziz <nasrulaev.a@usoft.uz>
2023-03-09 16:58:33 +0500

    first commit

---
+++ database/migrations/2023_03_08_202227_create_workers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('passport_series');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('position');
+            $table->integer('phone');
+            $table->string('address');
+            $table->string('company_id');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+};
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->create([
+
+            'name' => 'Admin',
+            'email' => 'admin@unplt.com',
+            'password' => bcrypt('password'),
+            'role' => 'Admin',
+
+        ]);
+    }
+}
